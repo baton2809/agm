e3202740534afe7177aef25cb88c2c4860f29e01
Artsiom Butomau <Artsiom_Butomau@epam.com>
2020-04-05 00:05:52 +0300

    Implement Add Course markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agm';
-  isAutenticated = false;
+  isAutenticated = true; // change to false
 
   authenticate() {
     this.isAutenticated = true;
