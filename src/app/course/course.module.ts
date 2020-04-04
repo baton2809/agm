685c30e4913d39fefd06cf89e6a7b20fdcccf850
Artsiom Butomau <Artsiom_Butomau@epam.com>
2020-04-04 23:26:10 +0300

    Add AOT, Presentation component on Course, add-course Component

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -6,6 +6,7 @@ import { ToolbarComponent } from './components/section/toolbar/toolbar.component
 import { SharedModule } from '../shared/shared.module';
 import { MoreComponent } from './components/section/course-list/more/more.component';
 import { SearchComponent } from './components/section/toolbar/search/search.component';
+import { AddCourseComponent } from './components/section/add-course/add-course.component';
 
 
 
@@ -20,6 +21,7 @@ import { SearchComponent } from './components/section/toolbar/search/search.comp
     ToolbarComponent,
     MoreComponent,
     SearchComponent,
+    AddCourseComponent,
   ],
   exports: [
     SectionComponent,
