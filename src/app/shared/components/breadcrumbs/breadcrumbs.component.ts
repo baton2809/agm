0e1ff275e886ed87bf2de9eb09fdd5fd9e86b147
Artsiom Butomau <Artsiom_Butomau@epam.com>
2020-05-02 15:35:34 +0300

    Add minor improvements

--- src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  private breadcrumb: string;
+  public breadcrumb: string;
   private rootPath: string;
   private id: string;
 
